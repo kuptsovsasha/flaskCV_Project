de8ab4515c2d2e2cbea700a7703d878633553390
kuptsovsasha <kuptsovsasha@gmail.com>
2023-03-23 11:25:13 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+
+from resources.cv_resource import CVResource
+from resources.job_title_resource import JobTitleResource
+
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(JobTitleResource, "/job-title")
+api.add_resource(CVResource, "/generate-cv")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
